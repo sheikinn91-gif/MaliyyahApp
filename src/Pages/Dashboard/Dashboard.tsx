2f1bcda5e9eb4abb2114a27d554a2c7d8074964c
sheikinn91-gif <sheikinn91@gmail.com>
2026-03-22 01:18:29 +0800

    fix: aktiviti terkini

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -183,9 +183,7 @@ export default function Dashboard() {
                       </td>
                       <td className="px-6 py-4">
                         <span className="text-[9px] bg-emerald-50 text-emerald-700 px-2 py-0.5 rounded-md font-black uppercase mb-1 inline-block">
-                          {item.kategori?.toUpperCase() === "PENDAPATAN"
-                            ? "JUMLAH ZAKAT"
-                            : item.kategori}
+                          JUMLAH ZAKAT
                         </span>
                         <p className="font-bold text-slate-800">
                           RM{" "}
